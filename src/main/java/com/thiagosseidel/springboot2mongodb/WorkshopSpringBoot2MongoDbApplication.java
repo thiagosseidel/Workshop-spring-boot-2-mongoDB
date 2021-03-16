ce90de56ca180d2412cae46ed30805d7367163a6
Thiago Seidel <thiagosseidel@gmail.com>
2021-03-16 20:10:39 -0300

    Project created

---
+++ src/main/java/com/thiagosseidel/springboot2mongodb/WorkshopSpringBoot2MongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.thiagosseidel.springboot2mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBoot2MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBoot2MongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagosseidel/springboot2mongodb/WorkshopSpringBoot2MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagosseidel.springboot2mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBoot2MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
