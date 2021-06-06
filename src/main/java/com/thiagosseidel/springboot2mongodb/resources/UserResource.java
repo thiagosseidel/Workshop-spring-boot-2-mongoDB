c1c463ffebd898233cf9218c980c648faef02492
Thiago Seidel <thiagosseidel@gmail.com>
2021-06-05 23:08:54 -0300

    Endpoint to return User Posts

--- src/main/java/com/thiagosseidel/springboot2mongodb/resources/UserResource.java
+++ src/main/java/com/thiagosseidel/springboot2mongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.thiagosseidel.springboot2mongodb.domain.Post;
 import com.thiagosseidel.springboot2mongodb.domain.User;
 import com.thiagosseidel.springboot2mongodb.dto.UserDTO;
 import com.thiagosseidel.springboot2mongodb.servicies.UserService;
@@ -76,5 +77,13 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
