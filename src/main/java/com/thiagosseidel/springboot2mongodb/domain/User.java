9b0fd112c23568207826f5362106c2355764ae2c
Thiago Seidel <thiagosseidel@gmail.com>
2021-06-05 23:02:31 -0300

    Referencing User Posts

--- src/main/java/com/thiagosseidel/springboot2mongodb/domain/User.java
+++ src/main/java/com/thiagosseidel/springboot2mongodb/domain/User.java
@@ -1,8 +1,11 @@
 package com.thiagosseidel.springboot2mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		super();
@@ -49,6 +55,18 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
+	public void addPosts(List<Post> posts) {
+		this.posts.addAll(posts);
 	}
 
 	@Override
